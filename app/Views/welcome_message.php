65fbc0581042800628fa592a3b33372372912c00
sergio <out_ssorias@findep.dev>
2023-08-29 23:53:58 -0700

    Prueba de commit y despliegue

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to Mikos <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Que Onda! Mikos <3 <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
